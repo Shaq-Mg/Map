0185c82f1daaa8967272f123ef061fa9fe8a77d6
Shaq-Mg <shaqmcg0@gmail.com>
2024-12-07 21:46:18 +0000

    Initial Commit

---
+++ Map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Map
+//
+//  Created by Shaquille McGregor on 07/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MapTests/MapTests.swift
@@ -0,0 +1,17 @@
+//
+//  MapTests.swift
+//  MapTests
+//
+//  Created by Shaquille McGregor on 07/12/2024.
+//
+
+import Testing
+@testable import Map
+
+struct MapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
